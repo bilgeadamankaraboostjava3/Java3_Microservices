ebbacab7dbb8ef34ebe837a1a2ff0c166f75f997
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-11-01 17:19:22 +0300

    01.11.2022 gün sonu

--- user-service/src/main/java/com/muhammet/repository/entity/UserProfile.java
+++ user-service/src/main/java/com/muhammet/repository/entity/UserProfile.java
@@ -4,19 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.*;
 
-@Table(name = "tbluserprofile")
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Document
 public class UserProfile {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    String id;
     /**
      * Auth servisinden kayıt olan kişinin auth id sini buraya eşitliyoruz.
      */
--- user-service/src/main/java/com/muhammet/service/UserProfileService.java
+++ user-service/src/main/java/com/muhammet/service/UserProfileService.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserProfileService extends ServiceManager<UserProfile,Long> {
+public class UserProfileService extends ServiceManager<UserProfile,String> {
 
     private final IUserProfileRepository iUserProfileRepository;
     private final JwtTokenManager tokenManager;
@@ -57,7 +57,7 @@ public class UserProfileService extends ServiceManager<UserProfile,Long> {
                 .username(dto.getUsername())
                 .email(dto.getEmail())
                 .build());
-       elasticSearchManager.save(userProfile);
+      // elasticSearchManager.save(userProfile);
         return true;
     }
     public Boolean update(UserProfileUpdateRequestDto dto){
